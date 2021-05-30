e098dd23a07079a282e565f416d7ce82dca2f1cc
RuneScaping <runescapop@proton.me>
2021-05-30 11:04:30 +0000

    Bump version to 0.3.6

--- api.py
+++ api.py
@@ -7,4 +7,7 @@ from transformers import (AlbertConfig,
                           )
 
 
-class Sent
+class SentimentAnalyzer:
+    def __init__(self, path='model', model_type='albert-base-v2'):
+        self.path = path
+        se
