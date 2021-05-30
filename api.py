3ff9818809727efa2abee45e2e616921b786b551
RuneScaping <runescapop@proton.me>
2021-05-30 12:37:18 +0000

    publishing

--- api.py
+++ api.py
@@ -10,4 +10,6 @@ from transformers import (AlbertConfig,
 class SentimentAnalyzer:
     def __init__(self, path='model', model_type='albert-base-v2'):
         self.path = path
-        se
+        self.model_type = model_type
+        self.tokenizer = AlbertTokenizer.from_pretrained(self.model_type, do_lower_case=True)
+        self.mode
