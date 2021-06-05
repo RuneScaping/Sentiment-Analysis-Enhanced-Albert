2edf1b69c8382e88c924720515ab4582877ae70b
RuneScaping <runescapop@proton.me>
2021-06-05 23:08:26 +0000

    Update yarn lock

--- api.py
+++ api.py
@@ -13,4 +13,9 @@ class SentimentAnalyzer:
         self.model_type = model_type
         self.tokenizer = AlbertTokenizer.from_pretrained(self.model_type, do_lower_case=True)
         self.model = AlbertForSequenceClassification.from_pretrained(self.path)
-        self.device = "cuda" if torch.cuda.is_available() el
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model.to(self.device)
+        self.model.eval()
+
+    def convert_to_features(self, sentence):
+   
