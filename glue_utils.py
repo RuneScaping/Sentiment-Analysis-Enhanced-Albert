5c8dc859842aadc303eab0f024a09b52967752b5
RuneScaping <runescapop@proton.me>
2023-01-13 21:51:26 +0000

    removes console.log

--- glue_utils.py
+++ glue_utils.py
@@ -2,4 +2,6 @@
 # Copyright 2018 The Google AI Language Team Authors and The HuggingFace Inc. team.
 # Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
 #
-# Licensed unde
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
