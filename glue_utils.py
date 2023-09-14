ce845c12977e6d7533ccac376f8c75508017560e
RuneScaping <runescapop@proton.me>
2023-09-14 10:03:31 +0000

    added missing method

--- glue_utils.py
+++ glue_utils.py
@@ -74,4 +74,9 @@ class InputFeatures(object):
 
     def __init__(self, input_ids, attention_mask, token_type_ids, label):
         self.input_ids = input_ids
-        self.attention_
+        self.attention_mask = attention_mask
+        self.token_type_ids = token_type_ids
+        self.label = label
+
+    def __repr__(self):
+        re
