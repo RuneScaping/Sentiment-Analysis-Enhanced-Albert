ad11fa450460020aab5f40367612144ba8b11cfb
RuneScaping <runescapop@proton.me>
2023-09-23 14:41:00 +0000

    Fix TOC

--- glue_utils.py
+++ glue_utils.py
@@ -98,4 +98,9 @@ class DataProcessor(object):
         """Gets an example from a dict with tensorflow tensors
 
         Args:
-            tensor_dict: Keys and values should match the 
+            tensor_dict: Keys and values should match the corresponding Glue
+                tensorflow_dataset examples.
+        """
+        raise NotImplementedError()
+
+    def 
