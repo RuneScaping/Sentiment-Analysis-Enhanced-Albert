7dbfb9f17007c82e9b07a9057e58af80dfe6843b
RuneScaping <runescapop@proton.me>
2023-09-28 19:52:38 +0000

    cni theme

--- glue_utils.py
+++ glue_utils.py
@@ -119,4 +119,9 @@ class DataProcessor(object):
         """Some tensorflow_datasets datasets are not formatted the same way the GLUE datasets are. 
         This method converts examples to the correct format."""
         if len(self.get_labels()) > 1:
-            example.label = self.get_labels()[int(examp
+            example.label = self.get_labels()[int(example.label)]
+        return example
+
+    @classmethod
+    def _read_tsv(cls, input_file, quotechar=None):
+        """Reads a tab
