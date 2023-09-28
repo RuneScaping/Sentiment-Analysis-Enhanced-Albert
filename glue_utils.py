1ce4080b2796ce9163b8f1ec3c0dc4cc729d08d2
RuneScaping <runescapop@proton.me>
2023-09-28 13:53:39 +0000

    files added

--- glue_utils.py
+++ glue_utils.py
@@ -117,4 +117,6 @@ class DataProcessor(object):
 
     def tfds_map(self, example):
         """Some tensorflow_datasets datasets are not formatted the same way the GLUE datasets are. 
-        This method converts examples
+        This method converts examples to the correct format."""
+        if len(self.get_labels()) > 1:
+            example.label = self.get_labels()[int(examp
