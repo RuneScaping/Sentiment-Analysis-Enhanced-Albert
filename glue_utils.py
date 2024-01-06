ca104da8db70c67375ad9c18da15cb5ce81cb499
RuneScaping <runescapop@proton.me>
2024-01-06 21:00:23 +0000

    updating .travis.yml

--- glue_utils.py
+++ glue_utils.py
@@ -195,4 +195,6 @@ def glue_convert_examples_to_features(examples, tokenizer,
 
         # Zero-pad up to the sequence length.
         padding_length = max_length - len(input_ids)
-        if pad_on_left
+        if pad_on_left:
+            input_ids = ([pad_token] * padding_length) + input_ids
+            attention_mask = ([0 if mask_padding_with_zero else
