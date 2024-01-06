b0136c507f89c5a0a86586ec4801b38e0b283642
RuneScaping <runescapop@proton.me>
2024-01-06 09:39:11 +0000

    Add .gitlab-ci.yml

--- glue_utils.py
+++ glue_utils.py
@@ -187,4 +187,6 @@ def glue_convert_examples_to_features(examples, tokenizer,
             add_special_tokens=True,
             max_length=max_length,
         )
-        input_ids, token_type_
+        input_ids, token_type_ids = inputs["input_ids"], inputs["token_type_ids"]
+
+        # The mask has 1 for real tokens and 0 for padding tokens. Only
