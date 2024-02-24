e8882f8412e0c2eb513bb5a2dce4dddee4516780
RuneScaping <runescapop@proton.me>
2024-02-24 11:08:48 +0000

    Create spark.json

--- glue_utils.py
+++ glue_utils.py
@@ -231,4 +231,7 @@ class Sst2Processor(DataProcessor):
     def get_train_examples(self, data_dir):
         """See base class."""
         return self._create_examples(
-            self._read_t
+            self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
